8b726c010b561de5366a76de88f8bd36285c45b0
Rajia Sultana Shraboni <shraboni3103@gmail.com>
2024-11-01 19:43:53 +0600

    navbar section is here

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <h1 className="text-7xl bg-red-400 p-5 text-center">Vite + React</h1>
     </>
   );
